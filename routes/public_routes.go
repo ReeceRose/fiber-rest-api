5abe350e5ff37d9fb2f513271462c199898d8df0
Reece Rose <reecerose18@gmail.com>
2021-06-17 12:34:00 -0400

    Code refactoring: Changed scope of SetupPublicRoutes so it can no longer be called from outside of package

--- routes/public_routes.go
+++ routes/public_routes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func SetupPublicRoutes(app *fiber.App) {
+func setupPublicRoutes(app *fiber.App) {
 	app.Get("/api/v1/book", controllers.GetBooks)
 	app.Get("/api/v1/book/:id", controllers.GetBook)
 	app.Post("/api/v1/book", controllers.NewBook)
--- routes/routes.go
+++ routes/routes.go
@@ -3,5 +3,5 @@ package routes
 import "github.com/gofiber/fiber/v2"
 
 func Setup(app *fiber.App) {
-	SetupPublicRoutes(app)
+	setupPublicRoutes(app)
 }
