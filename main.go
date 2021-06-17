c84b3935fb4e0956e9a9c278c01eaab8ee929b4a
Reece Rose <reecerose18@gmail.com>
2021-06-16 20:10:20 -0400

    Added basic book routes

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"github.com/ReeceRose/fiber-rest-api/book"
 	"github.com/gofiber/fiber/v2"
 )
 
-func helloWorld(c *fiber.Ctx) error {
-	return c.SendString("Hello world!")
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
 func main() {
 
 	app := fiber.New()
 
-	app.Get("/", helloWorld)
+	setupRoutes(app)
 
 	app.Listen(":3000")
 }
